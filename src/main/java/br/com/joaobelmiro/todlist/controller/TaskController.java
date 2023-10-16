4639a5b0e5d56f06d615a767b2e5bc2dd0751778
joão carlos belmiro gonçalves <joao_belmiro@hotmail.com>
2023-10-16 16:48:10 -0300

    endpoints para pegar dados

--- src/main/java/br/com/joaobelmiro/todlist/controller/TaskController.java
+++ src/main/java/br/com/joaobelmiro/todlist/controller/TaskController.java
@@ -2,12 +2,14 @@ package br.com.joaobelmiro.todlist.controller;
 
 import java.util.UUID;
 
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joaobelmiro.todlist.model.TasksEntity;
 import br.com.joaobelmiro.todlist.repository.TaskRepository;
+import br.com.joaobelmiro.todlist.repository.UserRepository;
 
 @RestController
 @RequestMapping("/task")
 public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/save")
     public ResponseEntity create(@RequestBody TasksEntity task) {
@@ -40,6 +46,26 @@ public class TaskController {
                     .body("Não existe ou não foi possivel alterar o conteúdo");
         }
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findById(@PathVariable String id) {
+        var task = taskRepository.findById(UUID.fromString(id)).orElse(null);
+        if (task != null) {
+            return ResponseEntity.ok().body(task);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi possivel retornar o recurso solicitado");
+    }
+
+    @GetMapping("/all-for-user/{idUser}")
+    public ResponseEntity findAllByUser(@PathVariable String idUser) {
+
+        var user = userRepository.findById(UUID.fromString(idUser)).orElse(null);
+        if (user != null) {
+            List allTasks = taskRepository.findByUserEntity(user);
+            return ResponseEntity.ok().body(allTasks);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O usuário não existe");
     }
 
     @DeleteMapping("/delete-task/")
--- src/main/java/br/com/joaobelmiro/todlist/repository/TaskRepository.java
+++ src/main/java/br/com/joaobelmiro/todlist/repository/TaskRepository.java
@@ -2,12 +2,15 @@ package br.com.joaobelmiro.todlist.repository;
 
 import java.util.UUID;
 
+import org.hibernate.mapping.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.joaobelmiro.todlist.model.TasksEntity;
+import br.com.joaobelmiro.todlist.model.UserEntity;
 
 @Repository
 public interface TaskRepository extends JpaRepository<TasksEntity, UUID> {
 
+    List findByUserEntity(UserEntity userEntity);
 }
